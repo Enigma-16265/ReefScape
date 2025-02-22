fede681ce74030cdb229e10728132e2fd9079dc4
Code Mac <code.mac@eenigmarobotics.org>
2025-02-22 14:50:14 -0700

    fix: (rj) added Epilogue initial

--- Poseidon/src/main/java/frc/robot/Robot.java
+++ Poseidon/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.epilogue.Epilogue;
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -15,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * described in the TimedRobot documentation. If you change the name of this class or the package after creating this
  * project, you must also update the build.gradle file in the project.
  */
+@Logged
 public class Robot extends TimedRobot
 {
 
@@ -28,6 +31,7 @@ public class Robot extends TimedRobot
   public Robot()
   {
     instance = this;
+    Epilogue.bind(this);
   }
 
   public static Robot getInstance()
