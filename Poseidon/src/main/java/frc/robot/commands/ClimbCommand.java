d10e56771b67daaaafb6f3e76fd1d8de985115e5
Code Mac <code.mac@eenigmarobotics.org>
2025-02-25 18:06:56 -0700

    fix: (rj) updated Climb subsystem

--- Poseidon/src/main/java/frc/robot/commands/ClimbCommand.java
+++ Poseidon/src/main/java/frc/robot/commands/ClimbCommand.java
@@ -4,7 +4,7 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Climb;
 
-public class ClimbController extends Command {
+public class ClimbCommand extends Command {
     private final Climb m_climb;
     private final DoubleSupplier m_speedSupplier;
 
@@ -14,7 +14,7 @@ public class ClimbController extends Command {
      * @param climb the Climb subsystem.
      * @param speedSupplier a supplier that provides a speed value (-1.0 to 1.0)
      */
-    public ClimbController(Climb climb, DoubleSupplier speedSupplier) {
+    public ClimbCommand(Climb climb, DoubleSupplier speedSupplier) {
         m_climb = climb;
         m_speedSupplier = speedSupplier;
         addRequirements(m_climb);
@@ -24,13 +24,13 @@ public class ClimbController extends Command {
     public void execute() {
         // Retrieve the speed value from the supplier and command the climb subsystem.
         double speed = m_speedSupplier.getAsDouble();
-        m_climb.setClimbSpeed(speed);
+        m_climb.setSpeed(speed);
     }
 
     @Override
     public void end(boolean interrupted) {
         // Stop the climb when the command ends or is interrupted.
-        m_climb.setClimbSpeed(0.0);
+        m_climb.setSpeed(0.0);
     }
 
     @Override
